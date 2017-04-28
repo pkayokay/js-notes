f8c890b865c8d6a18c44f4f0d38b26d3ef7cb797
Paul Kim <pkim.dev@gmail.com>
2017-04-28 14:47:14 -0400

    DOM manipulation

---
+++ dom_manipulation.js
@@ -0,0 +1,23 @@
+// DOM Manipulation
+// http://callmenick.com/post/basics-javascript-dom-manipulation
+// https://plainjs.com/javascript/manipulation/
+// http://www.hongkiat.com/blog/dom-manipulation-javascript-methods/
+
+// Create a DOM Element
+var element = document.createElement(tagName);
+
+// Add Child element to the DOM
+var parentElement = document.createElement("div");
+var childElement = document.createElement("p");
+
+parentElement.appendChild(childElement);
+
+// Styling an HTML Element
+element.style.color  "#000";
+element.style.marginTop = "30px";
+element.style.paddingBottom = "30px";
+
+// 
+
+
+
