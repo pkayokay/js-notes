6523fec427e9d8e3a3d92b109468b657aaadbcd6
Paul Kim <pkim.dev@gmail.com>
2017-07-22 22:21:40 -0400

    components

---
+++ react/3_component_notes.js
@@ -0,0 +1,11 @@
+
+Components
+Think of them as a collection of html css js and some internal data specific to that component
+They have everything you need in a bundle, they are either pure js or JSXif done is jsx, we need to compile and convert the jsx to js.
+
+Data is either received from a component's parent component, or it's contained in the component itself.
+The 'state' of the data for itself and the children components live in the parent component.
+We pass on the parent's data as an attribute for the children components to use this information
+
+
+
---
+++ react/babel_notes.js
@@ -0,0 +1,15 @@
+Babel compiles JavaScript.
+
+Webpack tells which transformations to make on your code
+Babel is the transformation itself.
+
+In React, it allows us to transform our JSX into JavaScript.
+We will add babel as a loader into our webpack settings, and our code will transform with what we have specified with Babel.
+
+
+
+
+
+
+
+
