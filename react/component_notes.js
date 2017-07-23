f9bb9b398f7b439e1cb039e0897733a606630606
Paul Kim <pkim.dev@gmail.com>
2017-07-22 22:27:45 -0400

    components

---
+++ react/component_notes.js
@@ -0,0 +1,18 @@
+
+// Components
+// Think of them as a collection of html css js and some internal data specific to that component
+// They have everything you need in a bundle, they are either pure js or JSXif done is jsx, we need to compile and convert the jsx to js.
+
+// Data is either received from a component's parent component, or it's contained in the component itself.
+// The 'state' of the data for itself and the children components live in the parent component.
+// We pass on the parent's data as an attribute for the children components to use this information
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+class HelloWorld extends React.Component {
+    render() {
+        return (
+            <div>Hello World!</div>
+        )
+    }
+}
