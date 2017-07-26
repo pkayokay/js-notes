44267b14da4f15febd79107964862044c5ad1294
Paul Kim <pkim.dev@gmail.com>
2017-07-26 13:58:26 -0400

    map and filter sample

--- react/props.js
+++ react/props.js
@@ -2,6 +2,27 @@
 // https://codepen.io/tylermcginnis/pen/RpvEvL
 // https://codepen.io/tylermcginnis/pen/NpoezB
 // https://codepen.io/tylermcginnis/pen/OpdreE
+  // map review
+  let digits = [3,2,3,4];
+  var addOne = digits.map(function(i) {
+    return i + 1
+  })
+  console.log(addOne);
+
+  var numbers = [1,2,3];
+  var numbersPlusTen = numbers.map(function(num) {
+    return num + 10
+  });
+  console.log(numbersPlusTen);
+
+  // filter review
+  var friends = ['Ean', 'Tyler', 'Mikenzi', 'Eric', 'Jessica'];
+  var newFriends = friends.filter(function (friend) {
+    return friend[0] === 'E'
+  });
+  console.log(newFriends) // ['Ean', 'Eric']
+
+
 
 class FriendsContainer extends React.Component {
   render() {
